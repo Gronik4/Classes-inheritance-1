bd754b29071f3f927fe6be634930d62ee449d7d4
Gronik64 <golegni@yandex.ru>
2021-11-05 15:06:57 +0300

    Правка синтаксис

--- src/js/Character.js
+++ src/js/Character.js
@@ -3,8 +3,11 @@ export default class Character {
     try {
       if ((name.length < 2) || (name.length > 10)) {
         throw new Error('Ошибка: в имени должно быть min - 2 символа, max - 10');
-      } else  { this.name = name; }
-    } catch(e) { return this.name = e.message }
+      } else { this.name = name; }
+    } catch(e) {
+      this.name = e.message;
+      return this.name;
+    }
 
     const charTypes = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
 
@@ -12,7 +15,10 @@ export default class Character {
       if (!charTypes.includes(type)) {
         throw new Error('Ошибка типа персонажа!!');
       } else { this.type = type; }
-    } catch(e) { return this.type = e.message;}
+    } catch(e) {
+      this.type = e.message;
+      return this.type;
+    }
     this.health = 100;
     this.level = 1;
     this.attack = undefined;
